279f4c11ca36fb5c7dab3e0aef7002abafec89b7
James Cook <mokus@deepbondi.net>
2012-03-20 10:43:22 -0400

    Promoted Dataset and Dataspace modules from the sandbox

---
+++ src/Bindings/HDF5/Dataset.hs
@@ -0,0 +1,210 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Bindings.HDF5.Dataset
+    ( Dataset
+    , createDataset
+    , createAnonymousDataset
+    , openDataset
+    , closeDataset
+    
+    , getDatasetSpace
+    , SpaceStatus(..)
+    , getDatasetSpaceStatus
+    , getDatasetType
+    
+    , getDatasetCreatePList
+    , getDatasetAccessPList
+    
+    , getDatasetStorageSize
+    , getDatasetOffset
+    
+    , readDataset
+    , readDatasetInto
+    , writeDataset
+    
+    , setDatasetExtent
+    ) where
+
+import Bindings.HDF5.Core
+import Bindings.HDF5.Dataspace
+import Bindings.HDF5.Datatype
+import Bindings.HDF5.Error
+import Bindings.HDF5.PropertyList.DAPL
+import Bindings.HDF5.PropertyList.DCPL
+import Bindings.HDF5.PropertyList.DXPL
+import Bindings.HDF5.PropertyList.LCPL
+import Bindings.HDF5.Raw.H5D
+import Bindings.HDF5.Raw.H5I
+import Bindings.HDF5.Raw.H5P
+import Bindings.HDF5.Raw.H5S
+import Control.Monad.ST (RealWorld)
+import qualified Data.ByteString as BS
+import qualified Data.Vector.Storable as SV
+import Foreign.Ptr.Conventions
+
+newtype Dataset = Dataset HId_t
+    deriving (Eq, HId, FromHId, HDFResultType)
+
+createDataset :: Location loc
+    => loc -> BS.ByteString -> TypeID -> Dataspace -> Maybe LCPL -> Maybe DCPL -> Maybe DAPL -> IO Dataset
+createDataset loc_id name type_id space_id lcpl_id dcpl_id dapl_id =
+    fmap Dataset $
+        withErrorCheck $
+            BS.useAsCString name $ \name ->
+                h5d_create2 (hid loc_id) name (hid type_id) (hid space_id) (maybe h5p_DEFAULT hid lcpl_id) (maybe h5p_DEFAULT hid dcpl_id) (maybe h5p_DEFAULT hid dapl_id)
+
+createAnonymousDataset :: Location loc
+    => loc -> TypeID -> Dataspace -> Maybe DCPL -> Maybe DAPL -> IO Dataset
+createAnonymousDataset loc_id type_id space_id dcpl_id dapl_id =
+    fmap Dataset $
+        withErrorCheck $
+            h5d_create_anon (hid loc_id) (hid type_id) (hid space_id) (maybe h5p_DEFAULT hid dcpl_id) (maybe h5p_DEFAULT hid dapl_id)
+
+openDataset :: Location loc
+    => loc -> BS.ByteString -> Maybe DAPL -> IO Dataset
+openDataset loc_id name dapl_id =
+    fmap Dataset $
+        withErrorCheck $
+            BS.useAsCString name $ \name -> 
+                h5d_open2 (hid loc_id) name (maybe h5p_DEFAULT hid dapl_id)
+
+closeDataset :: Dataset -> IO ()
+closeDataset (Dataset dset_id) =
+    withErrorCheck_ (h5d_close dset_id)
+
+getDatasetSpace :: Dataset -> IO Dataspace
+getDatasetSpace (Dataset dset_id) = 
+    fmap uncheckedFromHId $
+        withErrorCheck (h5d_get_space dset_id)
+
+data SpaceStatus
+    = NotAllocated
+    | Allocated
+    | PartAllocated
+    deriving (Eq, Ord, Read, Show, Enum, Bounded)
+
+spaceStatusFromCode :: H5D_space_status_t -> SpaceStatus
+spaceStatusFromCode c
+    | c == h5d_SPACE_STATUS_NOT_ALLOCATED   = NotAllocated
+    | c == h5d_SPACE_STATUS_ALLOCATED       = Allocated
+    | c == h5d_SPACE_STATUS_PART_ALLOCATED  = PartAllocated
+    | otherwise = error ("Unknown H5D_space_status_t " ++ show c)
+
+getDatasetSpaceStatus :: Dataset -> IO SpaceStatus
+getDatasetSpaceStatus (Dataset dset_id) =
+    fmap spaceStatusFromCode $
+        withOut_ $ \status -> 
+            withErrorCheck (h5d_get_space_status dset_id status)
+
+getDatasetType :: Dataset -> IO TypeID
+getDatasetType (Dataset dset_id) =
+    fmap TypeID $
+        withErrorCheck $
+            h5d_get_type dset_id
+
+getDatasetCreatePList :: Dataset -> IO DCPL
+getDatasetCreatePList (Dataset dset_id) =
+    fmap uncheckedFromHId $
+        withErrorCheck $
+            h5d_get_create_plist dset_id
+
+getDatasetAccessPList :: Dataset -> IO DAPL
+getDatasetAccessPList (Dataset dset_id) =
+    fmap uncheckedFromHId $
+        withErrorCheck $
+            h5d_get_access_plist dset_id
+
+getDatasetStorageSize :: Dataset -> IO HSize
+getDatasetStorageSize (Dataset dset_id) =
+    fmap HSize $
+        h5d_get_storage_size dset_id
+
+getDatasetOffset :: Dataset -> IO HAddr
+getDatasetOffset (Dataset dset_id) =
+    fmap HAddr $
+        withErrorCheck $
+            h5d_get_offset dset_id
+
+readDataset :: NativeType t =>
+    Dataset -> Maybe Dataspace -> Maybe DXPL -> IO (SV.Vector t)
+readDataset dset@(Dataset dset_id) file_space_id plist_id = do
+    effectiveSelection <- maybe (getDatasetSpace dset) return file_space_id
+    n <- getSimpleDataspaceExtentNPoints effectiveSelection
+    
+    withOutVector_ (fromIntegral n) $ \buf ->
+        withErrorCheck_ $
+            h5d_read dset_id (hdfTypeOf1 buf) h5s_ALL (maybe h5s_ALL hid file_space_id) (maybe h5p_DEFAULT hid plist_id) buf
+
+readDatasetInto :: NativeType t =>
+    Dataset -> Maybe Dataspace -> Maybe Dataspace -> Maybe DXPL -> SV.MVector RealWorld t -> IO ()
+readDatasetInto dset@(Dataset dset_id) mem_space_id file_space_id plist_id vec = do
+    effectiveSelection <- maybe (getDatasetSpace dset) return mem_space_id
+    n <- getSimpleDataspaceExtentNPoints effectiveSelection
+    
+    withOutMVector vec $ \vecSz buf ->
+        if fromIntegral n > vecSz
+            then fail "readDatasetInto: output vector is too small to contain selection"
+            else withErrorCheck_ $
+                    h5d_read dset_id (hdfTypeOf1 buf) (maybe h5s_ALL hid mem_space_id) (maybe h5s_ALL hid file_space_id) (maybe h5p_DEFAULT hid plist_id) buf
+
+writeDataset :: NativeType t =>
+    Dataset -> Maybe Dataspace -> Maybe Dataspace -> Maybe DXPL -> SV.Vector t -> IO ()
+writeDataset (Dataset dset_id) mem_space_id file_space_id plist_id buf =
+    withErrorCheck_ $
+        withInVector buf $ \buf ->
+            h5d_write dset_id (hdfTypeOf1 buf) (maybe h5s_ALL hid mem_space_id) (maybe h5s_ALL hid file_space_id) (maybe h5p_DEFAULT hid plist_id) buf
+
+-- foreign import ccall "wrapper" wrap_iterate_op
+--     :: (InOut a -> HId_t -> CUInt -> InArray HSize_t -> InOut b -> IO HErr_t)
+--     -> IO (H5D_operator_t a b)
+-- 
+-- TODO: figure out a good way to properly encapsulate the buffer so that
+-- out-of-bounds accesses are impossible.  Probably use a storable vector
+-- and check the bounds against the dataspace.
+-- Also check whether h5s_ALL is a valid dataspace; Actually I'm not sure how
+-- it could be, given that there is no information here about the size
+-- of the array apart from whatever is carried in the dataspace.
+-- iterateDatasetSelection :: NativeType t => InOutArray t -> Dataspace -> (InOut t -> [HSize] -> IO HErr_t) -> IO ()
+-- iterateDatasetSelection buf space_id op = do
+--     -- In order to marshall Haskell exceptions through the iterate operations,
+--     -- we use 'maxBound' as a "something might have happened" return value
+--     -- and pass a description of what that was via these IORefs.
+--     -- 
+--     -- The assertion mentioned here is that the 'type_id' argument to h5d_iterate
+--     -- is passed unchanged to the operator.  If that doesn't hold, then this
+--     -- function will need a more complicated type.
+--     assertionFailed <- newIORef False
+--     exception       <- newIORef Nothing :: IO (IORef (Maybe SomeException))
+--     
+--     let bufType = hdfTypeOf1 buf
+--     
+--     op <- wrap_iterate_op $ \elem type_id ndim (InArray point) _operator_data -> do
+--         point <- peekArray (fromIntegral ndim) point
+--         if type_id == bufType
+--             then do
+--                 result <- try (op elem (fmap HSize point))
+--                 case result of 
+--                     Left exc -> do
+--                         writeIORef exception (Just exc)
+--                         return maxBound
+--                     Right x -> return x
+--             else do
+--                 writeIORef assertionFailed True
+--                 return maxBound
+--     
+--     result <- withErrorCheck_
+--         (h5d_iterate buf bufType (hid space_id) op (InOut nullPtr)
+--         `finally` freeHaskellFunPtr op)
+--     
+--     when (result == maxBound) $ do
+--         assertionFailed <- readIORef assertionFailed
+--         when assertionFailed (throwIO (AssertionFailed "iterateDatasetSelection: operator called with different type_id than h5d_iterate was called with!"))
+--         exception <- readIORef exception
+--         maybe (return result) throwIO exception
+--         
+--     return result
+
+setDatasetExtent :: Dataset -> [HSize] -> IO ()
+setDatasetExtent (Dataset dset_id) sizes =
+    withErrorCheck_ $
+        withInList [sz | HSize sz <- sizes] $ \sizes ->
+            h5d_set_extent dset_id sizes
---
+++ src/Bindings/HDF5/Dataspace.hs
@@ -0,0 +1,372 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Bindings.HDF5.Dataspace
+    ( Dataspace
+    , DataspaceClass(..)
+    , SelectionOperator(..)
+    , SelectionType(..)
+    
+    , createDataspace
+    , createSimpleDataspace
+    , createExpandableDataspace
+    
+    , copyDataspace
+    
+    , closeDataspace
+    
+    , encodeDataspace
+    , decodeDataspace
+    
+    , getSimpleDataspaceExtentNPoints
+    , getSimpleDataspaceExtentNDims
+    , getSimpleDataspaceExtent
+    
+    , isSimpleDataspace
+    
+    , getDataspaceSelectionNPoints
+    
+    , selectHyperslab
+    , selectElements
+    
+    , getSimpleDataspaceExtentType
+    
+    , setDataspaceExtentNone
+    , copyDataspaceExtent
+    , dataspaceExtentsEqual
+    
+    , selectAll
+    , selectNone
+    
+    , offsetSimpleDataspaceSelection
+    
+    , selectionValid
+    
+    , getHyperslabSelection
+    , getHyperslabSelectionNBlocks
+    , getHyperslabSelectionBlockList
+    
+    , getElementSelection
+    , getElementSelectionNPoints
+    , getElementSelectionPointList
+    
+    , getSelectionBounds
+    
+    , getSelectionType
+    ) where
+
+import Bindings.HDF5.Core
+import Bindings.HDF5.Error
+import Bindings.HDF5.Raw.H5I
+import Bindings.HDF5.Raw.H5S
+import Control.Exception (assert)
+import Control.Monad
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Unsafe as BS
+import Data.List
+import Data.Maybe
+import qualified Data.Vector as V
+import qualified Data.Vector.Storable as SV
+import Foreign
+import Foreign.C
+import Foreign.Ptr.Conventions
+
+newtype Dataspace = Dataspace HId_t
+    deriving (Eq, HId, FromHId, HDFResultType)
+
+data DataspaceClass
+    = Scalar
+    | Simple
+    | Null
+    deriving (Eq, Ord, Enum, Bounded, Read, Show)
+
+rawDataspaceClass :: DataspaceClass -> H5S_class_t
+rawDataspaceClass Scalar = h5s_SCALAR
+rawDataspaceClass Simple = h5s_SIMPLE
+rawDataspaceClass Null   = h5s_NULL
+
+dataspaceClass :: H5S_class_t -> DataspaceClass
+dataspaceClass c
+    | c == h5s_SCALAR   = Scalar
+    | c == h5s_SIMPLE   = Simple
+    | c == h5s_NULL     = Null
+    | otherwise         = error ("unknown dataspace class: " ++ show c)
+
+createDataspace :: DataspaceClass -> IO Dataspace
+createDataspace cls = 
+    fmap Dataspace $
+        withErrorCheck $
+            h5s_create (rawDataspaceClass cls)
+
+createSimpleDataspace :: [HSize] -> IO Dataspace
+createSimpleDataspace dims = 
+    fmap Dataspace $
+        withErrorCheck $
+            withInList (map hSize dims) $ \dims ->
+                h5s_create_simple n dims (InArray nullPtr) 
+    where n = genericLength dims
+
+createExpandableDataspace :: [(HSize, Maybe HSize)] -> IO Dataspace
+createExpandableDataspace dims =
+    fmap Dataspace $
+        withErrorCheck $
+            withInList (map hSize dimSizes) $ \dimSizes -> 
+                withInList (map (maybe h5s_UNLIMITED hSize) dimLimits) $ \dimLimits ->
+                    h5s_create_simple n dimSizes dimLimits
+    where
+        n = genericLength dims
+        (dimSizes, dimLimits) = unzip dims
+
+copyDataspace :: Dataspace -> IO Dataspace
+copyDataspace (Dataspace space_id) =
+    fmap Dataspace $
+        withErrorCheck $
+            h5s_copy space_id
+
+closeDataspace :: Dataspace -> IO ()
+closeDataspace (Dataspace space_id) =
+    withErrorCheck_ $
+        h5s_close space_id
+
+encodeDataspace :: Dataspace -> IO BS.ByteString
+encodeDataspace (Dataspace space_id) = do
+    sz <- withInOut_ 0 $ \sz -> 
+        withErrorWhen_ (< 0) $
+            h5s_encode space_id (OutArray nullPtr) sz
+    
+    buf <- mallocBytes (fromIntegral sz)
+    sz <- withInOut_ sz $ \sz ->
+        withErrorWhen_ (< 0) $
+            h5s_encode space_id (OutArray buf) sz
+    
+    BS.unsafePackCStringLen (buf, fromIntegral sz)
+
+decodeDataspace :: BS.ByteString -> IO Dataspace
+decodeDataspace bs = BS.unsafeUseAsCString bs $ \buf -> 
+    fmap Dataspace $
+        withErrorCheck $
+            h5s_decode buf
+
+getSimpleDataspaceExtentNPoints :: Dataspace -> IO HSize
+getSimpleDataspaceExtentNPoints (Dataspace space_id) =
+    fmap fromIntegral $
+        withErrorWhen (< 0) $
+            h5s_get_simple_extent_npoints space_id
+
+getSimpleDataspaceExtentNDims :: Dataspace -> IO CInt
+getSimpleDataspaceExtentNDims (Dataspace space_id) =
+    withErrorWhen (< 0) $
+        h5s_get_simple_extent_ndims space_id
+
+getSimpleDataspaceExtent :: Dataspace -> IO ([HSize], [Maybe HSize])
+getSimpleDataspaceExtent space@(Dataspace space_id) = do
+    n <- getSimpleDataspaceExtentNDims space
+    
+    (dims, (maxDims, n')) <- 
+        withOutList (fromIntegral n) $ \dims ->
+            withOutList (fromIntegral n) $ \maxDims ->
+                withErrorWhen (< 0) $
+                    h5s_get_simple_extent_dims space_id dims maxDims
+    
+    assert (n==n') $ return
+        ( (map HSize dims)
+        , [ if d == h5s_UNLIMITED then Nothing else Just (HSize d) | d <- maxDims]
+        )
+
+isSimpleDataspace :: Dataspace -> IO Bool
+isSimpleDataspace (Dataspace space_id) =
+    htriToBool $
+        h5s_is_simple space_id
+
+getDataspaceSelectionNPoints :: Dataspace -> IO HSize
+getDataspaceSelectionNPoints (Dataspace space_id) =
+    fmap fromIntegral $
+        withErrorWhen (< 0) $
+            h5s_get_select_npoints space_id
+
+data SelectionOperator
+    = Set
+    | Or
+    | And
+    | Xor
+    | NotB
+    | NotA
+    | Append
+    | Prepend
+    deriving (Eq, Ord, Enum, Bounded, Read, Show)
+
+rawSelectionOperator :: SelectionOperator -> H5S_seloper_t
+rawSelectionOperator Set     = h5s_SELECT_SET
+rawSelectionOperator Or      = h5s_SELECT_OR
+rawSelectionOperator And     = h5s_SELECT_AND
+rawSelectionOperator Xor     = h5s_SELECT_XOR
+rawSelectionOperator NotB    = h5s_SELECT_NOTB
+rawSelectionOperator NotA    = h5s_SELECT_NOTA
+rawSelectionOperator Append  = h5s_SELECT_APPEND
+rawSelectionOperator Prepend = h5s_SELECT_PREPEND
+
+selectHyperslab :: Dataspace -> SelectionOperator -> [(HSize, Maybe HSize, HSize, Maybe HSize)] -> IO ()
+selectHyperslab space@(Dataspace space_id) oper hyperSlab = do
+    nDims <- getSimpleDataspaceExtentNDims space
+    
+    when (length hyperSlab /= fromIntegral nDims) 
+        (fail "selectHyperslab: the given hyperslab has the wrong number of dimensions for this dataspace")
+    
+    withErrorCheck_ $
+        withInList' start $ \start ->
+            maybe withNull withInList' stride $ \stride ->
+                withInList' count $ \count -> 
+                    maybe withNull withInList' block $ \block ->
+                        h5s_select_hyperslab space_id (rawSelectionOperator oper) start stride count block
+    where
+        (start, mbStrides, count, mbBlocks) = unzip4 hyperSlab
+        stride
+            | any isJust mbStrides  = Just (map (fromMaybe 1) mbStrides)
+            | otherwise             = Nothing
+        block
+            | any isJust mbBlocks  = Just (map (fromMaybe 1) mbBlocks)
+            | otherwise             = Nothing
+        withNull f = f (InArray nullPtr)
+        withInList' = withInList . map hSize
+
+selectElements :: Dataspace -> SelectionOperator -> V.Vector (SV.Vector HSize) -> IO ()
+selectElements space@(Dataspace space_id) oper elems = do
+    nDims <- getSimpleDataspaceExtentNDims space
+    
+    let nElems = V.length elems
+        packed = SV.generate (nElems * fromIntegral nDims) $ \i -> 
+            case i `divMod` nElems of
+                (dim, elt) -> elems V.! elt SV.! dim
+    
+    withErrorCheck_ $
+        withInVector packed $ \elems ->
+            h5s_select_elements space_id (rawSelectionOperator oper) (fromIntegral nElems) (castWrappedPtr elems)
+
+-- TODO: determine whether to throw an exception or simply report it
+-- when the returned value is invalid.
+getSimpleDataspaceExtentType :: Dataspace -> IO DataspaceClass
+getSimpleDataspaceExtentType (Dataspace space_id) =
+    fmap dataspaceClass $
+        withErrorWhen (\(H5S_class_t c) -> c < 0) $
+            h5s_get_simple_extent_type space_id
+
+setDataspaceExtentNone :: Dataspace -> IO ()
+setDataspaceExtentNone (Dataspace space_id) =
+    withErrorCheck_ $
+        h5s_set_extent_none space_id
+
+copyDataspaceExtent :: Dataspace -> Dataspace -> IO ()
+copyDataspaceExtent (Dataspace dst) (Dataspace src) =
+    withErrorCheck_ $
+        h5s_extent_copy dst src
+
+dataspaceExtentsEqual :: Dataspace -> Dataspace -> IO Bool
+dataspaceExtentsEqual (Dataspace s1) (Dataspace s2) =
+    htriToBool $
+        h5s_extent_equal s1 s2
+
+selectAll :: Dataspace -> IO ()
+selectAll (Dataspace space_id) =
+    withErrorCheck_ $
+        h5s_select_all space_id
+
+selectNone :: Dataspace -> IO ()
+selectNone (Dataspace space_id) =
+    withErrorCheck_ $
+        h5s_select_none space_id
+
+offsetSimpleDataspaceSelection :: Dataspace -> SV.Vector HSSize -> IO ()
+offsetSimpleDataspaceSelection space@(Dataspace space_id) offsets = do
+    nDims <- getSimpleDataspaceExtentNDims space
+    
+    when (fromIntegral nDims /= SV.length offsets)
+        (fail "offsetSimpleDataspaceSelection: offset vector's length must equal number dimensions in dataspace")
+    
+    withErrorCheck_ $ 
+        withInVector offsets $ \offsets ->
+            h5s_offset_simple space_id (castWrappedPtr offsets)
+
+selectionValid :: Dataspace -> IO Bool
+selectionValid (Dataspace space_id) =
+    htriToBool $
+        h5s_select_valid space_id
+
+getHyperslabSelection :: Dataspace -> IO (V.Vector (SV.Vector HSize, SV.Vector HSize))
+getHyperslabSelection space = getHyperslabSelectionBlockList space 0 maxBound
+
+getHyperslabSelectionNBlocks :: Dataspace -> IO HSize
+getHyperslabSelectionNBlocks (Dataspace space_id) =
+    fmap fromIntegral $
+        withErrorWhen (< 0) $
+            h5s_get_select_hyper_nblocks space_id
+
+getHyperslabSelectionBlockList :: Dataspace -> HSize -> HSize -> IO (V.Vector (SV.Vector HSize, SV.Vector HSize))
+getHyperslabSelectionBlockList space@(Dataspace space_id) startBlock numBlocks = do
+    n <- getHyperslabSelectionNBlocks space
+    nDims <- getSimpleDataspaceExtentNDims space
+    
+    let nBlks = min numBlocks (n - startBlock)
+        blkSize = 2 * fromIntegral nDims
+        numSizes = nBlks * blkSize
+        
+        
+    sizes <- withOutVector_ (fromIntegral numSizes) $ \buf ->
+        withErrorCheck_ $
+            h5s_get_select_hyper_blocklist space_id (hSize startBlock) (hSize nBlks) (castWrappedPtr buf)
+    
+    let decodePt i = SV.slice i (fromIntegral nDims) sizes
+        decodeBlock i = (decodePt b, decodePt (b + fromIntegral nDims))
+            where b = i * fromIntegral blkSize
+    return $! V.generate (fromIntegral nBlks) decodeBlock
+
+getElementSelection :: Dataspace -> IO (V.Vector (SV.Vector HSize))
+getElementSelection space = getElementSelectionPointList space 0 maxBound
+
+getElementSelectionNPoints :: Dataspace -> IO HSize
+getElementSelectionNPoints (Dataspace space_id) =
+    fmap fromIntegral $
+        withErrorWhen (< 0) $
+            h5s_get_select_elem_npoints space_id
+
+getElementSelectionPointList :: Dataspace -> HSize -> HSize -> IO (V.Vector (SV.Vector HSize))
+getElementSelectionPointList space@(Dataspace space_id) startPoint numPoints = do
+    n <- getElementSelectionNPoints space
+    nDims <- getSimpleDataspaceExtentNDims space
+    
+    let nPts = min numPoints (n - startPoint)
+        sz = nPts * fromIntegral nDims
+    
+    rawPoints <- withOutVector_ (fromIntegral sz) $ \buf ->
+        withErrorCheck_ $
+            h5s_get_select_elem_pointlist space_id (hSize startPoint) (hSize nPts) (castWrappedPtr buf)
+    
+    let unpackPt i = SV.generate (fromIntegral nDims) $ \j -> rawPoints SV.! (i + j * fromIntegral nPts)
+    
+    return $! V.generate (fromIntegral nPts) unpackPt
+
+getSelectionBounds :: Dataspace -> IO [(HSize, HSize)]
+getSelectionBounds space@(Dataspace space_id) = do
+    n <- getSimpleDataspaceExtentNDims space
+    
+    (start, end) <- 
+        withOutList (fromIntegral n) $ \start ->
+            withOutList_ (fromIntegral n) $ \end ->
+                withErrorCheck_ $
+                    h5s_get_select_bounds space_id start end
+    
+    return [(HSize s, HSize e) | (s,e) <- zip start end]
+
+data SelectionType
+    = Points
+    | Hyperslabs
+    deriving (Eq, Ord, Bounded, Enum, Read, Show)
+
+selectionType :: H5S_sel_type -> Maybe SelectionType
+selectionType c
+    | c == h5s_SEL_NONE         = Nothing
+    | c == h5s_SEL_POINTS       = Just Points
+    | c == h5s_SEL_HYPERSLABS   = Just Hyperslabs
+
+getSelectionType :: Dataspace -> IO (Maybe SelectionType)
+getSelectionType (Dataspace space_id) = 
+    fmap selectionType $
+        withErrorWhen (\(H5S_sel_type c) -> c < 0) $
+            h5s_get_select_type space_id
